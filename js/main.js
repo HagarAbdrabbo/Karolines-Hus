2d287df18a2b456bdb3dce8e9d20deae3e64296f
Hagar Abdrabbo <eaahhmar@students.eaaa.dk>
2018-12-11 19:17:11 +0100

    the mobile drop down menue close

--- js/main.js
+++ js/main.js
@@ -19,13 +19,15 @@ function initMap() {
 // drop down menue for the mobile virsion.
 let burger = document.querySelector("#burger-menu");
 let nav = document.querySelector("#mobile-nav");
-
+let disapper = document.querySelector("#main")
 
 burger.addEventListener("click", () => {
     nav.classList.add("visible");    
      console.log("Nav appered");
       });
    
-
-
-
+disapper.addEventListener("click", () => {
+    nav.classList.remove("visible");    
+     console.log("Nav disappered");
+      });
+   
