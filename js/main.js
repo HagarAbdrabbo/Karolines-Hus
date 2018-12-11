9596c1b5298b56c43391195f48097c39460d7f4c
Bruna <eaabrto@students.eaaa.dk>
2018-12-11 12:21:33 +0100

    location map

--- js/main.js
+++ js/main.js
@@ -8,10 +8,10 @@ document.addEventListener("DOMContentLoaded", function() {
 // Initialize and add the map
 function initMap() {
   // The location of Uluru
-  let uluru = {lat: -25.344, lng: 131.036};
+  let uluru = {lat: 56.8191588, lng: 9.010554500000012};
   // The map, centered at Uluru
   let map = new google.maps.Map(
-      document.getElementById('map'), {zoom: 4, center: uluru});
+      document.getElementById('map'), {zoom: 12, center: uluru});
   // The marker, positioned at Uluru
   let marker = new google.maps.Marker({position: uluru, map: map});
 }
