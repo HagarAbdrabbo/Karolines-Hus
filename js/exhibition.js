2625d6cb8dc75f9ecc42098d23d414bb0f355a68
Hagar Abdrabbo <eaahhmar@students.eaaa.dk>
2018-12-12 13:15:53 +0100

    changes

--- js/exhibition.js
+++ js/exhibition.js
@@ -3,42 +3,41 @@ document.addEventListener("DOMContentLoaded", function() {
   // Handler when the DOM is fully loaded
   console.log("Document ready!");
 
-
-/* artists pages individual */
-
-let urlString = window.location.href;
-    console.log(urlString);
-let url = new URL(urlString);
-let newsSlug = url.searchParams.get("news");
-console.log(newsSlug);
-
-fetch(`http://karolineshus.dk/wp-json/wp/v2/posts?_embed&slug=${newsSlug}`)
-.then(function(response){
-    return response.json();
-})
-.then(function(json){
-    console.log(json);
-    appendPosts(json);
 });
 
-function appendPosts(posts) {
-    for (let i = 0; i < posts.length; i++) {
-      let post = posts[i];
-      console.log(post);
- document.querySelector('#exhbition-post').innerHTML += `
-  <article>
- <h3>${post.title.rendered}</h3>            <p>${post.excerpt.rendered}</p>
+// featch the photographers page
+
+  fetch('http://karolineshus.dk/wp-json/wp/v2/posts?categories=24&_embed')
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(json) {
+      console.log(json);
+      appendPosts(json);
+    });
+
+
+    function appendPosts(posts) {
+        let html = "";
+        for (let i = 0; i < posts.length; i++) {
+        let post = posts[i];
+        console.log(post);
+        html += `
+         <a href="artist.html?artist=${post.slug}">
+          <article class="mutual-Artist-page" >
+            <h3>${post.title.rendered}</h3>
 <img src="${getFeaturedImageUrl(post)}" alt="${post.title.rendered}">
-</article>`;
+          </article></a>
+        `;
+      }
+    document.querySelector("#photographer-page").innerHTML = html;    
     }
-  }
-    
-     function getFeaturedImageUrl(post) {
+
+
+  function getFeaturedImageUrl(post) {
     let imageUrl = "";
     if (post._embedded['wp:featuredmedia']) {
       imageUrl = post._embedded['wp:featuredmedia'][0].source_url;
     }
     return imageUrl;
   }
-    
-});
